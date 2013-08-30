bd70a597b5e22cfec1ecd36195fb313e060faedb
Thorben Kroeger <thorben.kroeger@iwr.uni-heidelberg.de>
2013-08-30 18:15:47 +0200

    attempt to fix compilation on travis

--- vecvec_hdf5.hxx
+++ vecvec_hdf5.hxx
@@ -3,6 +3,16 @@
 
 #include "vecvec.hxx"
 
+//fix buggy hdf5 1.8 versions
+#include <H5version.h>
+#if (H5_VERS_MAJOR == 1 && H5_VERS_MINOR >= 8 && defined(H5_USE_16_API_DEFAULT))
+#define H5Gcreate_vers 2
+#define H5Gopen_vers 2
+#define H5Dopen_vers 2
+#define H5Dcreate_vers 2
+#define H5Acreate_vers 2
+#endif
+
 #include "hdf5.h"
 
 void writeHDF5(const VecVec& v, hid_t group, const char* dataset);
